6885f57ce07a3133da3754a21857003a956737c5
nikolay <ndikanskiy@gmail.com>
2021-09-16 17:54:35 +0300

    javabase_homework2 folder was added.

--- javabase_homework2/src/main/java/factory/implementations/FactoryImpl.java
+++ javabase_homework2/src/main/java/factory/implementations/FactoryImpl.java
@@ -10,7 +10,6 @@ public class FactoryImpl implements Factory {
 
     @Override
     public Notification getNotification(Class<? extends Notification> tClass, String body, User user) {
-
         if (tClass == PushNotification.class) {
             return new PushNotification(body, user);
         } else return new PhoneNotification(body, user);
